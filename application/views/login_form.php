ff26408e5a027761e56c3a58a8764ea425c042c4
Nichole Dwight <nicholedwight@gmail.com>
2014-05-14 11:19:59 +0100

    Styled login page

--- application/views/login_form.php
+++ application/views/login_form.php
@@ -1,1 +1,10 @@
-login form
+<div id="login_form">
+  <h1>Login, Fool!</h1>
+  <?php
+  echo form_open('login/validate_credentials');
+  echo form_input('username', 'Username');
+  echo form_password('password', 'Password');
+  echo form_submit('submit', 'Login');
+  echo anchor('login/register', 'Create Account');
+  ?>
+</div>

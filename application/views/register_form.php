7e50625985dd2b77af6ba6c378a70fae917d78a2
Nichole Dwight <nicholedwight@gmail.com>
2014-05-14 14:12:56 +0100

    Added register form styling and a redirect to login page

--- application/views/register_form.php
+++ application/views/register_form.php
@@ -1,28 +1,30 @@
 <h1>Create an Account</h1>
-<fieldset>
-  <legend>Personal Information</legend>
+<form class="register">
+  <h3>Personal Information</h3>
   <?php
   echo form_open('login/create_member');
   echo form_label('First Name', 'first_name');
-  echo form_input('first_name', set_value('first_name', 'First Name'));
+  echo form_input('first_name', set_value('first_name', ''));
   echo form_label('Last Name', 'last_name');
-  echo form_input('last_name', set_value('last_name', 'Last Name'));
+  echo form_input('last_name', set_value('last_name', ''));
   echo form_label('Email', 'email_address');
-  echo form_input('email_address', set_value('email_address', 'Email Address'));
+  echo form_input('email_address', set_value('email_address', ''));
   ?>
-</fieldset>
-<fieldset>
-  <legend>Login Info</legend>
+  <h3>Login Info</h3>
   <?php
   echo form_label('Username', 'username');
-  echo form_input('username', set_value('username', 'Username'));
+  echo form_input('username', set_value('username', ''));
   echo form_label('Password', 'password');
-  echo form_password('password', set_value('password', 'Password'));
+  echo form_password('password', set_value('password', ''));
   echo form_label('Confirm Password', 'password2');
-  echo form_password('password2', set_value('password2', 'Password'));
+  echo form_password('password2', set_value('password2', ''));
 
   echo form_submit('submit', 'Create Account');
   ?>
 
   <?php echo validation_errors('<p class="error">'); ?>
-</fieldset>
+</form>
+
+<div id="login-register-redirect">
+  <p>Already have an account?<a href="login/login">Login</a></p>
+</div>
